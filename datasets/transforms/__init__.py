8af34cf7714a3dfcea619623661dd973296a6a87
xypeng <pexure@gmail.com>
2022-06-27 10:54:28 +0800

    add tiny200

--- datasets/transforms/__init__.py
+++ datasets/transforms/__init__.py
@@ -1,5 +1,6 @@
 from .small import cifar_train_ccrop, cifar_train_rcrop, cifar_linear, cifar_test,\
-    stl10_train_rcrop, stl10_train_ccrop, stl10_linear, stl10_test
+    stl10_train_rcrop, stl10_train_ccrop, stl10_linear, stl10_test,\
+    tiny200_train_rcrop, tiny200_train_ccrop, tiny200_linear, tiny200_test
 from .imagenet import imagenet_pretrain_rcrop, imagenet_pretrain_ccrop, imagenet_linear_train, \
     imagenet_val, imagenet_eval_boxes
 
